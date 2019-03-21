598d502b08b31d43e44965fc572984e44eb83d6f
Tomas Akavickis <akavickistomas@gmail.com>
2019-03-21 22:58:16 +0000

    First commit

---
+++ TicTacoToe/TicTacoToe/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacoToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "x";
+            Console.WriteLine("    1   2   3");
+            Console.WriteLine("  |---|---|---|");
+            Console.WriteLine("1 | " + x + " | " + x + " | "+ x + " |");
+            Console.WriteLine("  |---|---|---|");
+            Console.WriteLine("2 | " + x + " | " + x + " | " + x + " |");
+            Console.WriteLine("  |---|---|---|");
+            Console.WriteLine("3 | " + x + " | " + x + " | " + x + " |");
+            Console.WriteLine("  |---|---|---|");
+
+            Console.ReadLine();
+        }
+    }
+}
